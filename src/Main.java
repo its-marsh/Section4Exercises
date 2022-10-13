5bce944593a6e6f0975136d70a0c013e0a8e7d4a
marsh <m.cowles21191@gmail.com>
2022-10-13 02:33:44 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(AreaCalculator.area(5.0));
+        MinutesToYearsAndDays.printYearsAndDays(525600);
+        MinutesToYearsAndDays.printYearsAndDays(1051200);
+        MinutesToYearsAndDays.printYearsAndDays(561600);
+
+        EqualityPrinter.printEqual(0, 1, 2);
+        System.out.println(PlayingCat.isCatPlaying(true, 10));
+        System.out.println(PlayingCat.isCatPlaying(false, 35));
     }
 }
